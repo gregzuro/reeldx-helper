e1fd5e8cffd4da1e800b0f6ef5c404989e1ba8a6
gregzuro <greg@zuro.net>
2015-03-11 12:45:01 -0700

    creates a one-hour token from app keys on current page

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,17 +1,20 @@
 try {
 	var pk= document.forms[0].elements[2].value ;
 	var sk= document.forms[0].elements[3].value ;
+	var app= document.forms[0].elements[0].value ;
 
 	chrome.runtime.sendMessage(
 		{
 			publicKey: pk,
-			secretKey: sk
+			secretKey: sk,
+			app: app
 		});
 }
 catch (e) {
 	chrome.runtime.sendMessage(
 		{
 			publicKey: 'n/a',
-			secretKey: 'n/a'
+			secretKey: 'n/a',
+			app: 'n/a'
 		});
 }
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -11,9 +11,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 chrome.runtime.onMessage.addListener(function( request) {
-    if (request.publicKey === 'n/a') {
-      renderStatus('Nothing to do here. :(') ;
-    } else {
-      renderStatus('Public Key: ' + request.publicKey + '\nSecret Key: ' + request.secretKey) ;
-    }
-	}) ;
+	if (request.publicKey === 'n/a') {
+		renderStatus('Nothing to do here. :(') ;
+	} else {
+		var algo = 'HS256' ;
+		var d= new Date();
+		var token = KJUR.jws.JWS.sign(
+			algo,
+			JSON.stringify({typ: 'JWT', alg: algo}),
+			JSON.stringify(
+				{
+				aud: request.publicKey,
+				iat: Math.floor(d.getTime()/1000),
+				exp: Math.floor(d.getTime()/1000) + (60*60),
+				sub: request.app
+			}, 'pass'),
+			request.secretKey) ;
+		renderStatus('Public Key: ' + request.publicKey + '\nSecret Key: ' + request.secretKey + '\nsub: '+ request.app +'\n1 Hour Token: ' + token) ;
+	}
+}) ;
